26088a2366b6af4a936b772687e8cc9278e5c582
bixler13 <bixler.brett@gmail.com>
2018-12-31 11:06:56 -0700

    added setup function that tests sd card

--- src/sdlog.cpp
+++ src/sdlog.cpp
@@ -5,7 +5,13 @@
 #include "sdlog.h"
 
 void sdlog_setup(){
-
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SDchip_pin)) {
+    Serial.println("SD Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
 }
 
 
