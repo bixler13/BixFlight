ce17fe48b13a23e2b1fc57c7f647a0cf671a76f5
bixler13 <bixler.brett@gmail.com>
2018-12-31 14:47:52 -0700

    added topper for the log file

--- src/sdlog.cpp
+++ src/sdlog.cpp
@@ -8,6 +8,15 @@ File log_file; //creating the file that will be written too in the sd card
 
 void sdlog_setup(){
   SD.begin(SDchip_pin);
+
+  log_file = SD.open("DATA.txt", FILE_WRITE);
+
+  if (log_file){
+    log_file.print(" dt ");
+    log_file.print(" , pitch ");
+    log_file.println(", roll");
+  }
+  log_file.close();
 }
 
 
