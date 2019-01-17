014b33baad4f679afbe37a16e8cdf33877c3fa1f
bixler13 <bixler.brett@gmail.com>
2019-01-17 14:30:54 -0700

    refined pid section

--- src/controller.cpp
+++ src/controller.cpp
@@ -14,53 +14,39 @@ void manual_mode(){
 //////////////////////////Pitch Axis/////////////////////////////
     //first we need to calculate error
     pitch_command = mapFloat(pitch_input,-1000,1000,-45,45);
+    roll_command = mapFloat(roll_input, -1000, 1000, -30,30);
     pitch_error = pitch_command-pitch;
+    roll_error = roll_command-roll;
 
     //Calculate the Kp porition
     P_pitch = p_pitch * pitch_error;
+    P_roll = p_roll * roll_error;
 
     //Calculate the Ki portion
       if(throttle_input >= -500){
       I_pitch_old = ((pitch_error * dt)+I_pitch_old);
       I_pitch_new = I_pitch_old *i_pitch;
+
+      I_roll_old = ((roll_error * dt)+I_roll_old);
+      I_roll_new = I_roll_old *i_roll;
       }
 
       else{
+        I_pitch_old = 0;
+        I_pitch_new = 0;
         I_pitch_old = 0;
         I_pitch_new = 0;
       }
 
     //Calculate the Kd portion
     D_pitch = ((pitch_error - pitch_error_old)/dt)*d_pitch;
-    pitch_error_old = pitch_error;
-
-    pitch_pidsum = (P_pitch + I_pitch_new + D_pitch); //sum the contributions
-    pitch_servo_angle = constrain(pitch_servo_center + pitch_pidsum, 30, 150); //take in account for the servo center (trim)
-
-    /////////////////////////Roll Axis//////////////////////////////
-    //first we need to calculate error
-    roll_command = mapFloat(roll_input, -1000, 1000, -30,30);
-    roll_error = roll_command-roll;
-
-    //Calculate the Kp porition
-    P_roll = p_roll * roll_error;
-
-    //Calculate the Ki portion
-    if(throttle_input >= -500){
-    I_roll_old = ((roll_error * dt)+I_roll_old);
-    I_roll_new = I_roll_old *i_roll;
-    }
-
-    else{
-    I_pitch_old = 0;
-    I_pitch_new = 0;
-    }
-
-    //Calculate the Kd portion
     D_roll = ((roll_error - roll_error_old)/dt)*d_roll;
+    pitch_error_old = pitch_error;
     roll_error_old = roll_error;
 
+    pitch_pidsum = (P_pitch + I_pitch_new + D_pitch); //sum the contributions
     roll_pidsum = (P_roll + I_roll_new + D_roll); //sum the contributions
+    pitch_servo_angle = constrain(pitch_servo_center + pitch_pidsum, 30, 150); //take in account for the servo center (trim)
     roll_servo_angle = constrain(roll_servo_center + roll_pidsum, 30, 150); //take in account for the servo center (trim)
     roll_servo2_angle = mapFloat(roll_servo_angle, 0, 180, 180 ,0);
     roll_servo_angle = mapFloat(roll_servo_angle, 0, 180, 180 ,0);
@@ -71,6 +57,7 @@ void manual_mode(){
      roll_rate_command = mapFloat(roll_input, -1000, 1000, -100, 100);
      pitch_rate = (pitch - pitch_old) / dt;
      roll_rate = (roll - roll_old) / dt;
+
 
      pitch = pitch_old;
      roll = roll_old;
