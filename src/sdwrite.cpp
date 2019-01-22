e4277b2ff25882d26e74ec4f7ddd65d63b192122
bixler13 <bixler.brett@gmail.com>
2019-01-22 16:10:34 -0700

    got it working real nice with strings

--- src/sdwrite.cpp
+++ src/sdwrite.cpp
@@ -3,37 +3,66 @@
 #include "sdwrite.h"
 #include "CircularBuffer.h"
 
-    SdFat Sd;
-    CircularBuffer<int,25> buffer;       // uses 319 bytes //we have 2000
+    SdFat sd;
+    SdFile csvFile;
+    int counter = 0;
+
 
     void sdwrite_setup(){
-      Sd.begin(4, SPI_FULL_SPEED); //SPI_FULL_SPEED
+      sd.begin(4, SPI_FULL_SPEED); //SD_SCK_MHZ(50)
+      csvFile.open("data.csv", O_CREAT | O_APPEND | O_WRITE);
     }
+
+    void sdlog(){
+      //csvFile.close();
+      csvFile.flush();
+      csvFile.open("data.csv", O_CREAT | O_APPEND | O_WRITE);
+      counter = 0;
+      }
 
     void sdwrite_loop(){
 
-          // Write to current buffer
-          //int dt = dt;
-          int roll_write = roll;
-          //int pitch_write = pitch;
-          //int yaw_write = yaw;
+        char dataString[5];
+        int roll_write = roll;
 
-          buffer.push(roll_write);
 
-          // If buffer full...
-          if(buffer.isFull()){
+        sprintf(dataString, "%d ,", roll_write);
 
-            String dataString = "";
+        csvFile.write(dataString);
 
-            for( int i = 0; i < 25; i++){ //conver to string
-            dataString += String(buffer[i]);
-            dataString += ",";
-            }
-
-            File dataFile = Sd.open("data.csv", O_CREAT | O_APPEND | O_WRITE);
-            dataFile.print(dataString);
-            dataFile.close();
-            dataFile.flush();
-            buffer.clear();
+          if(counter >= 10){
+            sdlog();
           }
+
+          counter ++;
+          Serial.println(counter);
         }
+
+
+        //CircularBuffer<int,25> buffer;       // uses 319 bytes //we have 2000
+
+        //
+        // // Write to current buffer
+        // //int dt = dt;
+        // int roll_write = roll;
+        // //int pitch_write = pitch;
+        // //int yaw_write = yaw;
+        //
+        // buffer.push(roll_write);
+        //
+        // // If buffer full...
+        // if(buffer.isFull()){
+        //
+        //   String dataString = "";
+        //
+        //   for( int i = 0; i < 25; i++){ //conver to string
+        //   dataString += String(buffer[i]);
+        //   dataString += ",";
+        //   }
+        //
+        //   File dataFile = Sd.open("data.csv", O_CREAT | O_APPEND | O_WRITE);
+        //   dataFile.print(dataString);
+        //   dataFile.close();
+        //   dataFile.flush();
+        //   buffer.clear();
+        // }
